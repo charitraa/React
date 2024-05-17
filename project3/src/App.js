feecfd49c744895708c8d5d46aa5e46afb915184
charitraa <stharabi9862187405@gmail.com>
2024-05-17 21:18:41 +0545

    project3 done

---
+++ project3/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Background from './Bacground'
+
+function App () {
+  return (
+    <div>
+      <Background />
+    </div>
+  )
+}
+
+export default App
---
+++ project3/src/Bacground.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function Background () {
+  return (
+    <>
+      <div className='hello'>
+        <div className='good'>Hello sir, Good Morning</div>
+      </div>
+    </>
+  )
+}
+
+export default Background
