414fff73fa45f32877bb0f1bac686b5fbea0e2b3
charitraa <stharabi9862187405@gmail.com>
2024-06-03 18:55:50 +0545

    chnage folders

--- Expense Tracker/Expense-Tracker/backend/Backend/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Transaction(models.Model):
-    name = models.CharField(max_length=200,null=False)
-    amount = models.CharField(max_length=200,null=False)
-    created_at = models.DateField(auto_created=True)
