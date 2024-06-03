8f7725083c5cb953d96820789c9c820668c1dbab
charitraa <stharabi9862187405@gmail.com>
2024-06-03 10:48:32 +0545

    delete

--- Expense Tracker/Expense-Tracker/frontend/src/App.js
+++ Expense Tracker/Expense-Tracker/frontend/src/App.js
@@ -1,23 +1,24 @@
-
-import './App.css';
-import Balance from './components/Balance';
-import Header from './components/Header';
-import Lists from './components/Lists';
-import Transaction from './components/Transaction';
-import {Context} from './Context/Context';
-function App() {
+import './App.css'
+import Balance from './components/Balance'
+import Header from './components/Header'
+import Lists from './components/Lists'
+import Transaction from './components/Transaction'
+import { Context } from './Context/Context'
+function App () {
   return (
     <>
-    <Context>
-    <div className="com">
-    <Header/>
-    <div className="balance">
-      <Balance/>
-      <Lists/>
-      <Transaction/>
-    </div></div>
-    </Context> </>
-  );
+      <Context>
+        <div className='com'>
+          <Header />
+          <div className='balance'>
+            <Balance />
+            <Lists />
+            <Transaction />
+          </div>
+        </div>
+      </Context>
+    </>
+  )
 }
 
-export default App;
+export default App
