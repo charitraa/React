896d380c623ee7b188ecc62e40af50f499fbb63a
charitraa <stharabi9862187405@gmail.com>
2024-06-03 18:54:17 +0545

    new

--- TodoList/Backend/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Todo(models.Model):
-    description = models.CharField(max_length=200)
-    created_at = models.DateField()
--- TodoList/Backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from Backend import views
-
-urlpatterns = [
-    path('todo-list/',views.todo_view,name='view'),
-    path('todo-add/',views.todo_add,name='add'),
-    path('todo-delete/<int:pk>/', views.todo_delete, name='delete')
-]
